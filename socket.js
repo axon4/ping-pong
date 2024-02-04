a45523ac259c08472be7eac662f64d67acb089dc
axon4 <65050809+axon4@users.noreply.github.com>
2024-02-04 13:54:27 +0000

    confine players to rooms to allow concurrent games

--- socket.js
+++ socket.js
@@ -2,30 +2,36 @@ let readyPlayers = 0;
 
 function listen(IO) {
 	const nameSpace = IO.of('/ping-pong');
+	let room;
 
 	nameSpace.on('connection', socket => {
 		console.log('client connected:', socket.id);
 
 		socket.on('ready', () => {
-			console.log('player ready:', socket.id);
+			console.log('player ready:', socket.id, '|', 'room:', room);
+
+			room = 'room' + Math.floor(readyPlayers / 2);
+			socket.join(room);
 
 			readyPlayers++;
 
 			if (readyPlayers % 2 === 0) {
-				nameSpace.emit('start', socket.id);
+				nameSpace.in(room).emit('start', socket.id);
 			};
 		});
 
 		socket.on('paddleMove', paddle => {
-			socket.broadcast.emit('paddleMove', paddle);
+			socket.to(room).emit('paddleMove', paddle);
 		});
 
 		socket.on('ballMove', ball => {
-			socket.broadcast.emit('ballMove', ball);
+			// socket.broadcast.emit('ballMove', ball);
+			socket.to(room).emit('ballMove', ball);
 		});
 
 		socket.on('disconnect', reason => {
 			console.log(`client ${socket.id} disconnected: ${reason}`);
+			socket.leave(room);
 		});
 	});
 };
